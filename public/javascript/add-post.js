46abc088920a1c7c2487ecfda61b150edf27e9cc
Brian <brian.lockerbie@gmail.com>
2022-07-24 21:04:29 -0700

    created add-post

---
+++ public/javascript/add-post.js
@@ -0,0 +1,30 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('textarea[name="post-body"]').value;
+
+    if(title && content) {
+  
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+    } else {
+        alert("Please complete all empty areas.")
+    }
+  }
+  
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
