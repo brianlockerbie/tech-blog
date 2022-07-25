369a5b1ad3a343d0ac09d8946c23beca45e63bb0
Brian <brian.lockerbie@gmail.com>
2022-07-24 22:28:59 -0700

    added javascript post, comment, edit, login, logout, signup

---
+++ public/javascript/signup.js
@@ -0,0 +1,29 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      // check the response status
+      if (response.ok) {
+        console.log('success');
+        alert('Congradulations! Your account has been created!')
+        document.location.replace('/login');
+      } else {
+        alert(response.statusText);
+      }
+    }
+};
+
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
